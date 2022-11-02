de8ffc962d417bef8abf0e74513a87ce4f67ad8e
Natalia Torres <nataliatorres88@gmail.com>
2022-11-02 16:57:56 -0500

    modified the app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Welcome from './pages/welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="http://drive.google.com/uc?export=view&id=1e97q_y-fHOu6r_91jjBfbHmLyPiKD5XR" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome/>
     </div>
   );
 }
