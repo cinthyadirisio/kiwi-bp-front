0f8e388c2cf08471433e7ba154bb73414a55416c
Natalia Torres <nataliatorres88@gmail.com>
2022-11-02 16:22:33 -0500

    app created

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="http://drive.google.com/uc?export=view&id=1e97q_y-fHOu6r_91jjBfbHmLyPiKD5XR" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
