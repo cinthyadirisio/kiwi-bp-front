90be5fc32b5ebc1284d78af2b400af75aea038d7
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-11-03 13:03:39 -0300

    added Reactswitch for darkmode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './pages/welcome'
+import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
+
+
+export const ThemeContext = createContext("light");
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((e) => (e === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{ theme, toggleTheme }} >
+      <div className="App" id={theme}>
       <Welcome/>
-    </div>
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    
+
+
+
+    </ThemeContext.Provider>
   );
 }
 
